47ed43fb596374b5db694af7fc9e9a3eef61cb96
unknown <arjenandeweg@hotmail.com>
2022-06-02 14:54:51 +0200

    small bugfixes

--- src/container/Skills/Skills.jsx
+++ src/container/Skills/Skills.jsx
@@ -26,7 +26,7 @@ const Skills = () => {
   return (
     <>
       <h2 className="head-text">
-        Skills & <span>Experiences</span>
+        Skills & <span>Courses</span>
       </h2>
 
       <div className="app__skills-container">
